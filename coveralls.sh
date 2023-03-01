8e1222e18405209993ce9bb1ee0264b15aa1a165
Valentin Kiselev <mrexox@evilmartians.com>
2023-03-01 13:03:20 +0300

    fix: support carryforward

--- coveralls.sh
+++ coveralls.sh
@@ -1,21 +1,30 @@
 #!/usr/bin/env bash
 
 download() {
-  set -x
-  # TODO: Determine the download way by RUNNER_OS
-  curl -L https://coveralls.io/coveralls-linux.tar.gz | tar xz
-  set +x
+  [[ "$COVERALLS_DEBUG" == "true" ]] && set -x
+  if [[ "$RUNNER_OS" == "macOS" ]]
+  then
+    brew tap coverallsapp/coveralls
+    brew install coveralls
+  elif [[ "$RUNNER_OS" == "Windos" ]]
+  then
+    # TODO: Add a step for windows
+  else
+    curl -L https://coveralls.io/coveralls-linux.tar.gz | tar xz -C /usr/local/bin
+  fi
+  [[ "$COVERALLS_DEBUG" == "true" ]] && set +x
 }
 
 call() {
   if [[ -n $COVERALLS__DONE ]]; then
-    ./coveralls --done
+    coveralls --done
   else
     local args=""
     [[ -n $COVERALLS__FILE ]] && args="$args --file $COVERALLS__FILE"
     [[ -n $COVERALLS__BASE_PATH ]] && args="$args --base-path $COVERALLS__BASE_PATH"
+    [[ "$COVERALLS_DEBUG" == "true" ]] && args="$args --debug"
 
-    ./coveralls $args
+    coveralls $args
   fi
 }
 
